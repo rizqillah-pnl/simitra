eab9fd28b5b63c932404c4a5b18709b80709efa5
Rizka Rahmadini Salim <rizkarahmadini13@gmail.com>
2022-08-16 11:06:28 +0700

    Nambahin data base

--- model/lowongan.php
+++ model/lowongan.php
@@ -5,18 +5,18 @@ include '../controller/validateText.php';
 session_start();
 $kode = $_SESSION['id'];
 
-$sql = mysqli_query($conn, "SELECT a.Kode_petugas, a.Username, a.Email, a.Password, a.Old_password, a.Last_login, a.Status, a.Created_at, a.Updated_at, b.Nama, b.NIK, b.Alamat, b.Foto, b.NoHP, b.Tanggal_lahir, b.Tempat_lahir, c.Jabatan, c.Id_jabatan FROM auth a, petugas b, jabatan c WHERE a.Kode_petugas=b.Kode_petugas AND b.Jabatan=c.Id_jabatan AND a.Kode_petugas='$kode'");
+$sql = mysqli_query($conn, "SELECT a.Kode_petugas, a.Username, a.Email, a.Password, a.Old_password, a.Last_login, a.Created_at, a.Updated_at, b.Nama, b.NIK, b.Alamat, b.Foto, b.NoHP, b.Tanggal_lahir, b.Tempat_lahir, c.Jabatan, c.Id_jabatan FROM auth a, petugas b, jabatan c WHERE a.Kode_petugas=b.Kode_petugas AND b.Jabatan=c.Id_jabatan AND a.Kode_petugas='$kode'");
 $result = mysqli_fetch_assoc($sql);
 
 if (isset($_POST['daftarLowongan'])) {
     $idLowongan = $_POST['idLowongan'];
-
-    $cari = mysqli_query($conn, "SELECT * FROM tb_lowongan_user WHERE id='$idLowongan' AND id_user='$kode'");
+    
+    $cari = mysqli_query($conn, "SELECT * FROM tb_lowongan_user WHERE id='$idLowongan' AND id_petugas='$kode'");
     if (mysqli_num_rows($cari) == 0) {
 
         date_default_timezone_set('Asia/Jakarta');
         $now = date("Y-m-d H-i-s");
-        $daftar = mysqli_query($conn, "INSERT INTO tb_lowongan_user (id_lowongan, id_petugas, tanggal_daftar) VALUES ('$idLowongan', '$kode', '$now')");
+        $daftar = mysqli_query($conn, "INSERT INTO tb_lowongan_user (id_lowongan, id_petugas, id_kec, tanggal_daftar) VALUES ('$idLowongan', '$kode', $id_kec', '$now')");
 
         if ($daftar) {
             $_SESSION['pesan'] = "berhasil";
--- simitrabpsacut.sql
+++ simitrabpsacut.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 02, 2022 at 10:26 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.29
+-- Waktu pembuatan: 16 Agu 2022 pada 06.04
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,11 +24,12 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `auth`
+-- Struktur dari tabel `auth`
 --
 
 CREATE TABLE `auth` (
   `Kode_petugas` int(10) NOT NULL,
+  `Id_jabatan` int(11) NOT NULL,
   `Username` varchar(25) NOT NULL,
   `Email` varchar(100) DEFAULT NULL,
   `Password` varchar(50) NOT NULL,
@@ -39,18 +40,24 @@ CREATE TABLE `auth` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `auth`
+-- Dumping data untuk tabel `auth`
 --
 
-INSERT INTO `auth` (`Kode_petugas`, `Username`, `Email`, `Password`, `Old_password`, `Last_login`, `Created_at`, `Updated_at`) VALUES
-(1, 'ila', 'rahmaini@gmail.com', 'aafe26449a364e5d6b5db7dc565a9b6a', 'aafe26449a364e5d6b5db7dc565a9b6a', '2022-08-01 13:54:02', '2022-07-08 07:55:50', '2022-07-27 15:44:10'),
-(23, 'admin', 'admin@geobase.com', '21232f297a57a5a743894a0e4a801fc3', NULL, '2022-08-02 10:16:23', '2022-07-19 07:41:01', '2022-08-02 09:44:26'),
-(57, 'rizqillah', NULL, 'aafe26449a364e5d6b5db7dc565a9b6a', NULL, '2022-08-02 09:18:43', '2022-08-02 08:16:07', NULL);
+INSERT INTO `auth` (`Kode_petugas`, `Id_jabatan`, `Username`, `Email`, `Password`, `Old_password`, `Last_login`, `Created_at`, `Updated_at`) VALUES
+(1, 1, 'ila', 'rahmaini@gmail.com', 'aafe26449a364e5d6b5db7dc565a9b6a', 'aafe26449a364e5d6b5db7dc565a9b6a', '2022-08-15 10:54:45', '2022-07-08 07:55:50', '2022-07-27 15:44:10'),
+(23, 0, 'admin', 'admin@geobase.com', '21232f297a57a5a743894a0e4a801fc3', NULL, '2022-08-15 21:39:12', '2022-07-19 07:41:01', '2022-08-02 09:44:26'),
+(57, 0, 'rizqillah', NULL, 'aafe26449a364e5d6b5db7dc565a9b6a', NULL, '2022-08-02 09:18:43', '2022-08-02 08:16:07', NULL),
+(62, 0, 'sqxa', NULL, 'd41d8cd98f00b204e9800998ecf8427e', NULL, '2022-08-13 20:32:16', '2022-08-13 20:32:16', NULL),
+(63, 0, 'srs', NULL, 'd41d8cd98f00b204e9800998ecf8427e', NULL, '2022-08-13 20:39:14', '2022-08-13 20:39:14', NULL),
+(64, 0, 'survei', NULL, 'd41d8cd98f00b204e9800998ecf8427e', NULL, '2022-08-13 20:43:56', '2022-08-13 20:43:56', NULL),
+(65, 0, 'er', NULL, 'd41d8cd98f00b204e9800998ecf8427e', NULL, '2022-08-13 20:44:29', '2022-08-13 20:44:29', NULL),
+(66, 0, 'asa', NULL, '457391c9c82bfdcbb4947278c0401e41', NULL, '2022-08-14 20:19:04', '2022-08-14 20:19:04', NULL),
+(67, 0, 'rizka', 'rizkarahmadini13@gmail.com', 'aef2c231d5e776c0e0656eaf68767848', NULL, '2022-08-16 11:02:41', '2022-08-15 11:00:45', '2022-08-15 21:42:35');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `jabatan`
+-- Struktur dari tabel `jabatan`
 --
 
 CREATE TABLE `jabatan` (
@@ -59,7 +66,7 @@ CREATE TABLE `jabatan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `jabatan`
+-- Dumping data untuk tabel `jabatan`
 --
 
 INSERT INTO `jabatan` (`Id_jabatan`, `Jabatan`) VALUES
@@ -69,7 +76,51 @@ INSERT INTO `jabatan` (`Id_jabatan`, `Jabatan`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `lowongan`
+-- Struktur dari tabel `kecamatan`
+--
+
+CREATE TABLE `kecamatan` (
+  `id_kec` varchar(6) NOT NULL,
+  `nama_kec` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data untuk tabel `kecamatan`
+--
+
+INSERT INTO `kecamatan` (`id_kec`, `nama_kec`) VALUES
+('010', 'SAWANG'),
+('020', 'NISAM'),
+('021', 'NISAM ANTARA'),
+('022', 'BANDA BARO'),
+('030', 'KUTAMAKMUR'),
+('031', 'SIMPANG KEURAMAT'),
+('040', 'SYAMTALIRA BAYU'),
+('041', 'GEUREUDONG PASE'),
+('050', 'MEURAH MULIA'),
+('060', 'MATANGKULI'),
+('061', 'PAYABAKONG'),
+('062', 'PIRAK TIMU'),
+('070', 'COT GIREK'),
+('080', 'TANAH JAMBO AYE'),
+('081', 'LANGKAHAN'),
+('090', 'SEUNUDDON'),
+('100', 'BAKTIYA'),
+('101', 'BAKTIYA BARAT'),
+('110', 'LHOKSUKON'),
+('120', 'TANAH LUAS'),
+('121', 'NIBONG'),
+('130', 'SAMUDERA'),
+('140', 'SYAMTALIRA ARON'),
+('150', 'TANAH PASIR'),
+('151', 'LAPANG'),
+('160', 'MUARA SATU'),
+('170', 'DEWANTARA');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `lowongan`
 --
 
 CREATE TABLE `lowongan` (
@@ -83,12 +134,11 @@ CREATE TABLE `lowongan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `lowongan`
+-- Dumping data untuk tabel `lowongan`
 --
 
 INSERT INTO `lowongan` (`id`, `jenis_lowongan`, `tanggal_mulai`, `tanggal_akhir`, `persyaratan`, `deskripsi`, `gambar`) VALUES
 (1, 'Survei Ekonomi', '2022-01-11', '2022-01-26', 'adas', 'asdasdas', '4.png'),
-(2, 'Survei Pertanian', '2022-01-02', '2022-08-24', 'asdsad', 'assdasads', '13.png'),
 (3, 'Sensus penduduk', '2022-08-03', '2022-08-12', 'asdasd', 'asdsa', '5.png'),
 (4, 'Survei Pekerjaan', '2022-08-01', '2022-09-10', 'dsadsad', 'asdasd', '27.png'),
 (5, 'Survei SBH', '2022-08-01', '2022-09-01', 'sdaasdasd', 'sadsadasd', '103.png');
@@ -96,7 +146,7 @@ INSERT INTO `lowongan` (`id`, `jenis_lowongan`, `tanggal_mulai`, `tanggal_akhir`
 -- --------------------------------------------------------
 
 --
--- Table structure for table `petugas`
+-- Struktur dari tabel `petugas`
 --
 
 CREATE TABLE `petugas` (
@@ -113,116 +163,129 @@ CREATE TABLE `petugas` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `petugas`
+-- Dumping data untuk tabel `petugas`
 --
 
 INSERT INTO `petugas` (`Kode_petugas`, `NIK`, `Nama`, `Foto`, `Jabatan`, `Tempat_lahir`, `Tanggal_lahir`, `Alamat`, `NoHP`, `Jkel`) VALUES
 (1, NULL, 'Rahmaini', NULL, 2, NULL, NULL, NULL, NULL, 'P'),
 (23, '2132132132', 'Admin', NULL, 1, '', '0000-00-00', '', '', 'L'),
-(57, NULL, 'rizqillah', NULL, 2, NULL, NULL, NULL, NULL, 'L');
+(57, NULL, 'rizqillah', NULL, 2, NULL, NULL, NULL, NULL, 'L'),
+(67, '1408105309010001', 'Rizka', '2022-08-15 21-42-35line_1045775003259953-min.jpg', 2, '', '2022-08-17', '', '081364011325', 'L');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_lowongan_user`
+-- Struktur dari tabel `tb_lowongan_user`
 --
 
 CREATE TABLE `tb_lowongan_user` (
   `id` int(11) NOT NULL,
   `id_lowongan` int(11) NOT NULL,
   `id_petugas` int(12) NOT NULL,
+  `id_kec` varchar(6) CHARACTER SET latin1 NOT NULL,
   `tanggal_daftar` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tb_lowongan_user`
+-- Dumping data untuk tabel `tb_lowongan_user`
 --
 
-INSERT INTO `tb_lowongan_user` (`id`, `id_lowongan`, `id_petugas`, `tanggal_daftar`) VALUES
-(1, 1, 57, '2022-08-02 09:10:37');
+INSERT INTO `tb_lowongan_user` (`id`, `id_lowongan`, `id_petugas`, `id_kec`, `tanggal_daftar`) VALUES
+(1, 3, 57, '010', '2022-08-16 05:17:25');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `auth`
+-- Indeks untuk tabel `auth`
 --
 ALTER TABLE `auth`
-  ADD PRIMARY KEY (`Kode_petugas`);
+  ADD PRIMARY KEY (`Kode_petugas`),
+  ADD KEY `Id_jabatan` (`Id_jabatan`);
 
 --
--- Indexes for table `jabatan`
+-- Indeks untuk tabel `jabatan`
 --
 ALTER TABLE `jabatan`
-  ADD PRIMARY KEY (`Id_jabatan`);
+  ADD PRIMARY KEY (`Id_jabatan`),
+  ADD KEY `Id_jabatan` (`Id_jabatan`);
 
 --
--- Indexes for table `lowongan`
+-- Indeks untuk tabel `kecamatan`
+--
+ALTER TABLE `kecamatan`
+  ADD PRIMARY KEY (`id_kec`) USING BTREE;
+
+--
+-- Indeks untuk tabel `lowongan`
 --
 ALTER TABLE `lowongan`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `petugas`
+-- Indeks untuk tabel `petugas`
 --
 ALTER TABLE `petugas`
   ADD PRIMARY KEY (`Kode_petugas`),
   ADD KEY `Jabatan` (`Jabatan`);
 
 --
--- Indexes for table `tb_lowongan_user`
+-- Indeks untuk tabel `tb_lowongan_user`
 --
 ALTER TABLE `tb_lowongan_user`
   ADD PRIMARY KEY (`id`),
   ADD KEY `lowongan` (`id_lowongan`),
-  ADD KEY `petugas_lowongan` (`id_petugas`);
+  ADD KEY `petugas_lowongan` (`id_petugas`),
+  ADD KEY `id_kec` (`id_kec`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `auth`
+-- AUTO_INCREMENT untuk tabel `auth`
 --
 ALTER TABLE `auth`
-  MODIFY `Kode_petugas` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
+  MODIFY `Kode_petugas` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
 
 --
--- AUTO_INCREMENT for table `jabatan`
+-- AUTO_INCREMENT untuk tabel `jabatan`
 --
 ALTER TABLE `jabatan`
   MODIFY `Id_jabatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `lowongan`
+-- AUTO_INCREMENT untuk tabel `lowongan`
 --
 ALTER TABLE `lowongan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT for table `tb_lowongan_user`
+-- AUTO_INCREMENT untuk tabel `tb_lowongan_user`
 --
 ALTER TABLE `tb_lowongan_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `petugas`
+-- Ketidakleluasaan untuk tabel `petugas`
 --
 ALTER TABLE `petugas`
   ADD CONSTRAINT `Auth` FOREIGN KEY (`Kode_petugas`) REFERENCES `auth` (`Kode_petugas`) ON UPDATE CASCADE,
   ADD CONSTRAINT `Jabatan` FOREIGN KEY (`Jabatan`) REFERENCES `jabatan` (`Id_jabatan`) ON UPDATE CASCADE;
 
 --
--- Constraints for table `tb_lowongan_user`
+-- Ketidakleluasaan untuk tabel `tb_lowongan_user`
 --
 ALTER TABLE `tb_lowongan_user`
   ADD CONSTRAINT `lowongan` FOREIGN KEY (`id_lowongan`) REFERENCES `lowongan` (`id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `petugas_lowongan` FOREIGN KEY (`id_petugas`) REFERENCES `petugas` (`Kode_petugas`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `petugas_lowongan` FOREIGN KEY (`id_petugas`) REFERENCES `petugas` (`Kode_petugas`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `tb_lowongan_user_ibfk_1` FOREIGN KEY (`id_kec`) REFERENCES `kecamatan` (`id_kec`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tb_lowongan_user_ibfk_2` FOREIGN KEY (`id_kec`) REFERENCES `kecamatan` (`id_kec`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
